85ac78e8f3d4bf330fbb0c27ab0912b2e4197e20
Jonathan Chen <jchen2019@pingry.org>
2017-03-30 12:24:27 -0400

    Added average code to SumAndAverage.py.

--- SumAndAverage.py
+++ SumAndAverage.py
@@ -7,5 +7,14 @@ sum = 0
 for number in fileContents:
 	sum = sum + int(number)
 	
-print("The sum of the numbers is " + str(sum)) + "."
+print("The sum of the numbers is " + str(sum) + ".")
+
+totalNumbers = 0
+
+for number in fileContents:
+	totalNumbers += 1
+	
+average = sum / totalNumbers
+
+print("The average of the numbers is " + str(average) + "."
 
